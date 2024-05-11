74ca4e9273befd0d671f308521ef870393aa286e
Priscyla Santos <santospriscyla@gmail.com>
2024-05-11 13:59:15 -0300

    Inserindo arquivo de confuguração

---
+++ config.py
@@ -0,0 +1,13 @@
+GOOGLE_API_KEY = "GOOGLE_API_KEY"
+
+GENERATION_CONFIG = {
+    "candidate_count": 1,
+    "temperature": 0.5,
+}
+
+SAFETY_SETTINGS = {
+    "HARASSMENT": "BLOCK_NONE",
+    "HATE": "BLOCK_NONE",
+    "SEXUAL": "BLOCK_NONE",
+    "DANGEROUS": "BLOCK_NONE",
+}
